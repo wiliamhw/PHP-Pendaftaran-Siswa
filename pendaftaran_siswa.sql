815081b1c8256cc751dc6a4c746aec51f2140610
William H.W <wiliamwijaya1985@gmail.com>
2020-10-29 23:41:02 +0700

    Add database

---
+++ pendaftaran_siswa.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 29, 2020 at 05:40 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pendaftaran_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=swe7;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$1RJOo7Tl61bfPBP1vjEcBug1lissFoKE1SIf.Q899P3p9Bvx0UBEW');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `calon_siswa`
+--
+
+CREATE TABLE `calon_siswa` (
+  `id` int(100) UNSIGNED NOT NULL,
+  `nama` varchar(64) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `agama` varchar(10) NOT NULL,
+  `sekolah_asal` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=swe7;
+
+--
+-- Dumping data for table `calon_siswa`
+--
+
+INSERT INTO `calon_siswa` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`) VALUES
+(1, 'Lord Dapos', 'Jl. Lolipop', 'Laki-laki', 'Islam', 'SMPN 21 Surabaya'),
+(3, 'William Handi Wijaya', 'Jl. Isekai', 'Laki-laki', 'Kristen', 'SMPN 88 Mondstadt'),
+(4, 'Jean', 'Jl. Monstadt2', 'Perempuan', 'Katolik', 'SMPN 67'),
+(5, 'Lord Kosimp', 'Jl. Kesuburan420', 'Laki-laki', 'Katolik', 'SMPN Virtual Novel');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=swe7;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'user', '$2y$10$1c4PK5JQxUdQASsRl5VfieGZUQDo/AIrmn68MSHSNB3CUoFwYj68u');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  MODIFY `id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
