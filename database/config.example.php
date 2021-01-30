6ac857c9a5dfbb21af32dd138ab9070468a661f8
William <wiliamwijaya1985@gmail.com>
2021-01-30 17:17:25 +0700

    Improve db security

---
+++ database/config.example.php
@@ -0,0 +1,12 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$config = [
+    'database' => [
+        'name' => '',
+        'username' => '',
+        'password' => '',
+        'connection' => '',
+        'options' => []
+    ]
+];
